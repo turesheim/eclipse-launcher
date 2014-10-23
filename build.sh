62da43cb49cf390be27af88c6201340d3e1be1a3
Torkild U. Resheim <torkildr@gmail.com>
2014-10-23 10:52:03 +0200

    Only run Sonar build if bundle build finished OK.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mvn clean verify -f no.resheim.eclipse.utils-parent 
-mvn sonar:sonar -Psonar -f no.resheim.eclipse.utils-parent
+if [ $? -eq 0 ]; then
+	mvn sonar:sonar -Psonar -f no.resheim.eclipse.utils-parent
+fi
