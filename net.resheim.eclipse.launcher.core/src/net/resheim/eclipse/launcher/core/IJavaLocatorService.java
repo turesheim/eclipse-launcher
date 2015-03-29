327917221671362ecf10d46a4e3afbd9d31925f9
Torkild U. Resheim <torkildr@gmail.com>
2015-03-29 20:48:10 +0200

    Update copyright strings.

--- net.resheim.eclipse.launcher.core/src/net/resheim/eclipse/launcher/core/IJavaLocatorService.java
+++ net.resheim.eclipse.launcher.core/src/net/resheim/eclipse/launcher/core/IJavaLocatorService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Torkild U. Resheim.
+ * Copyright (c) 2014-2015 Torkild U. Resheim.
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
--- net.resheim.eclipse.launcher.macosx/src/net/resheim/eclipse/launcher/macosx/handlers/Messages.java
+++ net.resheim.eclipse.launcher.macosx/src/net/resheim/eclipse/launcher/macosx/handlers/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Torkild U. Resheim and others
+ * Copyright (c) 2012-2015 Torkild U. Resheim and others
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
