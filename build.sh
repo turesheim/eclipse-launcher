7c181fae258f136c8506b26d22d9c678bbd0694b
Torkild U. Resheim <torkildr@gmail.com>
2015-03-31 20:23:54 +0200

    Fix #5275: Make UI-tests work again with specific target platform
    Task-Url:
    https://github.com/turesheim/eclipse-utilities/issues/issue/5275

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Perform the actual build with unit tests
-mvn clean install -Pskip-ui-tests -f net.resheim.eclipse.launcher-parent 
-# Execute UI tests
-if [ $? -eq 0 ]; then
-	mvn verify -f net.resheim.eclipse.launcher.tests
-fi
+mvn clean verify -f net.resheim.eclipse.launcher-parent 
