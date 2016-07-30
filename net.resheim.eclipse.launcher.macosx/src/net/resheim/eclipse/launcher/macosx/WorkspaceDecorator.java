34c2a3772e0a15ad311ba19c86a3b2a0830a70df
Torkild U. Resheim <torkildr@gmail.com>
2016-07-30 22:12:08 +0200

    Specifically use the task item from the application.

--- net.resheim.eclipse.launcher.macosx/src/net/resheim/eclipse/launcher/macosx/WorkspaceDecorator.java
+++ net.resheim.eclipse.launcher.macosx/src/net/resheim/eclipse/launcher/macosx/WorkspaceDecorator.java
@@ -1,25 +1,26 @@
 /*******************************************************************************
- * Copyright (c) 2012-2015 Torkild U. Resheim.
- * 
+ * Copyright (c) 2012-2016 Torkild U. Resheim.
+ *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors: Torkild U. Resheim - initial API and implementation
  *******************************************************************************/
 package net.resheim.eclipse.launcher.macosx;
 
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TaskBar;
 import org.eclipse.swt.widgets.TaskItem;
+import org.eclipse.ui.PlatformUI;
 
 import net.resheim.eclipse.launcher.core.IWorkspaceDecorator;
 
 /**
- * Obtains the {@link TaskItem} belonging to the running application and sets the badge to the workspace name.
- * 
+ * Obtains the {@link TaskItem} belonging to the running application and sets
+ * the badge to the workspace name.*
+ *
  * @author Torkild U. Resheim
  */
 public class WorkspaceDecorator implements IWorkspaceDecorator {
@@ -28,16 +29,11 @@ public class WorkspaceDecorator implements IWorkspaceDecorator {
 	}
 
 	private static TaskItem getTaskBarItem() {
-		Display display = Display.getDefault();
-		Shell shell = display.getActiveShell();
-		TaskBar bar = display.getSystemTaskBar();
+		TaskBar bar = PlatformUI.getWorkbench().getDisplay().getSystemTaskBar();
 		if (bar == null) {
 			return null;
 		}
-		TaskItem item = bar.getItem(shell);
-		if (item == null) {
-			item = bar.getItem(null);
-		}
+		TaskItem item = bar.getItem(null);
 		return item;
 	}
 
